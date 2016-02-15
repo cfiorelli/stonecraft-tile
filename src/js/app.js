f9121cb8ebb07b4054c19a96ed2996a2963d61a3
Daniel Emery <danieljemery@gmail.com>
2016-02-15 13:30:17 -0800

    Initial commit. Mobile layout and functionality complete.

---
+++ src/js/app.js
@@ -0,0 +1,27 @@
+// Show gallery on click
+$('.gallery-reveal').on('click', function(){
+	$(this).siblings('.test-wrap').slideToggle();
+});
+
+// Change category header background clicked gallery image
+$('.slide').on('click', function(evt) {
+  evt.preventDefault();
+  var $newImage = $(this).attr('href');
+  $(this).parent().parent().siblings('.category-header').css('background-image', 'url(' + $newImage + ')');
+});
+
+// Smooth Scrolling for same=page anchors
+// $(function() {
+//   $('a[href*="#"]:not([href="#"])').click(function() {
+//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+//       var target = $(this.hash);
+//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+//       if (target.length) {
+//         $('html, body').animate({
+//           scrollTop: target.offset().top
+//         }, 1000);
+//         return false;
+//       }
+//     }
+//   });
+// });
